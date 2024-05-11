243629207f5ae29374265d6c80d75a554bdc579c
LeBerek <weberska@gmail.com>
2024-05-11 08:06:56 +0200

    Committing file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/SearchPhrase_Chunk6.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/SearchPhrase_Chunk5.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/SearchPhrase_Chunk4.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/SearchPhrase_Chunk3.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/SearchStem_Chunk1.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/SearchPhrase_Chunk2.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/SearchPhrase_Chunk1.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Resources/Scripts/require.config.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/Tocs/Master_Chunk0.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/Tocs/Master.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/SearchUrl_Chunk0.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/SearchTopic_Chunk0.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/SearchStem_Chunk0.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/SearchPhrase_Chunk0.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/Search.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/HelpSystem.xml file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/Data/HelpSystem.js file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/index.mcwebhelp file:///C:/Dyplom/Nadzywczajne-Przewodnik_2.0/docs/HTML5 - Top Navigation.4.log.zip

--- docs/Data/Search.js
+++ docs/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/Exceptional Consignments/Definition.htm'],s:['+','line'],p:[['+',2],['both',22],['example',10],['keep',35],['own',24],['swivel',7],['weight',38]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/Exceptional Consignments/Definition.htm'],s:['+','line'],p:[['+',2],['bottom',5],['example',22],['kinematic',37],['owner',37],['symmetrical',34],['what',12]]});
--- docs/Resources/Scripts/require.config.js
+++ docs/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638510111405183814'
+    urlArgs: 't=638510115366893192'
 });
